83e95daa430eb6732880d6b9db773ac7a6bea34d
Feliphe Paz <feliphepaz@MacBook-Air-de-Feliphe.local>
2022-07-16 13:50:47 -0300

    rotas definidas

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,76 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const Slugify = require("slugify");
 
-router.get("/categories", (req, res) => {
-  res.send("Rota de categorias");
+router.get("/admin/categories/new", (req, res) => {
+  res.render("admin/categories/new");
 });
 
-router.get("/categories/category/new", (req, res) => {
-  res.send("Rota para criar novas categorias");
+router.get("/admin/categories/edit/:id", (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    Category.findByPk(id).then((category) => {
+      res.render("admin/categories/edit", { category });
+    });
+  }
+});
+
+router.post("/categories/save", (req, res) => {
+  const title = req.body.title;
+  if (title) {
+    Category.create({
+      title,
+      slug: Slugify(title),
+    }).then(() => {
+      res.redirect("/admin/categories");
+    });
+  } else {
+    res.redirect("/admin/categories/new");
+  }
+});
+
+router.post("/categories/edit", (req, res) => {
+  const title = req.body.title;
+  const id = req.body.id;
+  if (title) {
+    Category.update(
+      {
+        title,
+        slug: Slugify(title),
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    ).then(() => {
+      res.redirect("/admin/categories");
+    });
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
+router.get("/categories/delete/:id", (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    Category.destroy({
+      where: {
+        id,
+      },
+    }).then(() => {
+      res.redirect("/admin/categories");
+    });
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
+router.get("/admin/categories", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/categories/index", { categories });
+  });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const Article = require("./articles/Article");
 const Category = require("./categories/Category");
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
