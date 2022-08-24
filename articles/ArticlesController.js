932e5647cd11e3dbf2ff5af5c5c2c4edb59ed36e
Feliphe Paz <feliphepaz@MacBook-Air-de-Feliphe.local>
2022-08-24 17:57:23 -0300

    salvando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const Article = require("../articles/Article");
+const Category = require("../categories/Category");
+const Slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
+router.get("/admin/articles", (req, res) => {
   res.send("Rota de artigos");
 });
 
-router.get("/articles/article/new", (req, res) => {
-  res.send("Rota para criar novos artigos");
+router.get("/admin/articles/new", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories });
+  });
+});
+
+router.post("/articles/save", (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  const category = req.body.category;
+  if (title && body && category) {
+    Article.create({
+      title,
+      slug: Slugify(title),
+      body,
+      categoriaId: category,
+    }).then(() => {
+      res.redirect("/admin/articles");
+    });
+  } else {
+    res.redirect("/admin/articles");
+  }
 });
 
 module.exports = router;
