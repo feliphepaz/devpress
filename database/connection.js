31268b1cc527cdefea5dfff3776798f11b0d8c45
Feliphe Paz <feliphe.s.paz@gmail.com>
2023-11-20 13:44:44 -0300

    refactor: dotenv project

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,15 @@
+require("dotenv").config({ path: ".env.local" });
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("devpress", "root", "MercenaryMusty@9719", {
-  host: "localhost",
-  dialect: "mysql",
-  timezone: "-03:00",
-});
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    timezone: "-03:00",
+  }
+);
 
 module.exports = connection;
